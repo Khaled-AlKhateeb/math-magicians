ea45fa7c74bd06dbe2af3948f814ab0ea322244e
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2022-10-28 14:25:19 +0200

    fix hook issues

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,45 +2,48 @@ import React, { useState } from 'react';
 import calculate from './logic/calculate';
 
 const Calculator = () => {
-  const [count, setCount] = useState(0);
+  const [object, setObject] = useState({
+    total: 0,
+    next: 0,
+    operation: null,
+  });
 
-  useState = () => {
-    const obj = calculate(count, setCount);
-    setCount(obj);
+  const handleChange = (e) => {
+    const keyValue = e.target.innerHTML;
+    setObject(calculate(object, keyValue));
   };
 
-  const { total, next, operation } = setCount;
   return (
     <div className="calc-container">
       <div className="result-container">
         <div className="result">
           <span>
-            {total}
-            {operation}
-            {next}
+            {object.total}
+            {object.operation}
+            {object.next}
           </span>
         </div>
       </div>
       <div className="keys-container">
-        <button onClick={setCount} type="button" className="calc-keys">AC</button>
-        <button onClick={setCount} type="button" className="calc-keys">+/-</button>
-        <button onClick={setCount} type="button" className="calc-keys">%</button>
-        <button onClick={setCount} type="button" className="calc-keys arithmetic">&#247;</button>
-        <button onClick={setCount} type="button" className="calc-keys">7</button>
-        <button onClick={setCount} type="button" className="calc-keys">8</button>
-        <button onClick={setCount} type="button" className="calc-keys">9</button>
-        <button onClick={setCount} type="button" className="calc-keys arithmetic">x</button>
-        <button onClick={setCount} type="button" className="calc-keys">4</button>
-        <button onClick={setCount} type="button" className="calc-keys">5</button>
-        <button onClick={setCount} type="button" className="calc-keys">6</button>
-        <button onClick={setCount} type="button" className="calc-keys arithmetic">-</button>
-        <button onClick={setCount} type="button" className="calc-keys">1</button>
-        <button onClick={setCount} type="button" className="calc-keys">2</button>
-        <button onClick={setCount} type="button" className="calc-keys">3</button>
-        <button onClick={setCount} type="button" className="calc-keys arithmetic">+</button>
-        <button onClick={setCount} type="button" className="calc-keys zero-key">0</button>
-        <button onClick={setCount} type="button" className="calc-keys">.</button>
-        <button onClick={setCount} type="button" className="calc-keys arithmetic">=</button>
+        <button onClick={handleChange} type="button" className="calc-keys">AC</button>
+        <button onClick={handleChange} type="button" className="calc-keys">+/-</button>
+        <button onClick={handleChange} type="button" className="calc-keys">%</button>
+        <button onClick={handleChange} type="button" className="calc-keys arithmetic">&#247;</button>
+        <button onClick={handleChange} type="button" className="calc-keys">7</button>
+        <button onClick={handleChange} type="button" className="calc-keys">8</button>
+        <button onClick={handleChange} type="button" className="calc-keys">9</button>
+        <button onClick={handleChange} type="button" className="calc-keys arithmetic">x</button>
+        <button onClick={handleChange} type="button" className="calc-keys">4</button>
+        <button onClick={handleChange} type="button" className="calc-keys">5</button>
+        <button onClick={handleChange} type="button" className="calc-keys">6</button>
+        <button onClick={handleChange} type="button" className="calc-keys arithmetic">-</button>
+        <button onClick={handleChange} type="button" className="calc-keys">1</button>
+        <button onClick={handleChange} type="button" className="calc-keys">2</button>
+        <button onClick={handleChange} type="button" className="calc-keys">3</button>
+        <button onClick={handleChange} type="button" className="calc-keys arithmetic">+</button>
+        <button onClick={handleChange} type="button" className="calc-keys zero-key">0</button>
+        <button onClick={handleChange} type="button" className="calc-keys">.</button>
+        <button onClick={handleChange} type="button" className="calc-keys arithmetic">=</button>
       </div>
     </div>
   );
